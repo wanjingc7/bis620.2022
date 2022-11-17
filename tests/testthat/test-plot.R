d9a1c1e9747f30afe64667c3fe4c66355cf0ad1c
wanjingc7 <wanjing.c7@gmail.com>
2022-11-17 13:44:33 -0500

    add test for plot function with frequency data

--- tests/testthat/test-plot.R
+++ tests/testthat/test-plot.R
@@ -20,6 +20,17 @@ test_that(
   {
     data(ukb_accel)
     p <- accel_plot(ukb_accel[1:100, ])
-    vdiffr::expect_doppelganger("first-100-samples", p)
+    vdiffr::expect_doppelganger("first-100-time-samples", p)
+  }
+)
+
+test_that(
+  "The accel_plot() is correct for frequency data.",
+  {
+    data(ukb_accel)
+    p <- ukb_accel[1:100, ]|>
+      spectral_signature()|>
+      accel_plot()
+    vdiffr::expect_doppelganger("first-100-freq-samples", p)
   }
 )
